0bc7fdc54460babe3d2b6ca7a52a29729105fe8c
phucvan44 <93427029+phucvan44@users.noreply.github.com>
2021-11-03 14:40:57 +0700

    Python Advance

--- Python_nangcao/quanlinhanvien.py
+++ Python_nangcao/quanlinhanvien.py
@@ -1,193 +1,186 @@
-class Info:
-	def __init__(self, name, birth, possition, skill, start, language, project,id):
-		self.name = name
-		self.birth = birth 
-		self.possition = possition
-		self.skill = skill
-		self.start = start
-		self.language = language
-		self.project = project
-		self.id = id
+from datetime import datetime
 
-class NhanVien:
-	list_nv = []
-	def __init__(self, n):
-		self.n = n 
-		self.list_nv = []
+class Employee_Info:
+	def __init__(self, **kwargs):
+		for employee_key, employee_value in kwargs.items():
+			setattr(self, employee_key, employee_value)
 
-	def enter_nv(self, id):
-		print("Nhân viên thứ: ", id+1)
 
-		print("\t Họ và tên: ", end="")
-		name = str(input())
+class Learn_Employee:
+	list_employee = []
 
-		print("\t Năm sinh: ", end="")
-		birth = int(input())
+	def add_employee(self, info_employee):
+		self.list_employee = info_employee
 
-		print("\t Vị trí công việc hiện tại: ", end="")
-		possition = str(input())
-
-		print("\t Năm bắt đầu làm việc: ", end="")
-		start = int(input())
-
-		print("\t Số kĩ năng hiện có: ", end="")
-		n_skill = int(input())
-		list_skill = []
-		for i in range(n_skill):
-			print("\t\t Kĩ năng {0}: ".format(i+1), end="")
-			sub = str(input())
-			list_skill.append(sub)
-
-		print("\t Số lượng ngôn ngữ lập trình đã dùng: ", end="")
-		n_lang = int(input())
-		list_lang = [] 
-		for i in range(n_lang):
-			print("\t\t Ngôn ngữ {0}: ".format(i+1), end="")
-			sub = str(input())
-			list_lang.append(sub)
-
-		print("\t Số lượng project đã hoàn thành: ", end="")
-		n_pro = int(input())
-		list_pro = [] 
-		for i in range(n_pro):
-			print("\t\t Project {0}: ".format(i+1), end="")
-			sub = str(input())
-			list_pro.append(sub)
-
-		info_nv = Info(name, birth, possition, list_skill, start, list_lang, list_pro, id)
-		self.list_nv.append(info_nv)
-		print("-"*50)
-
-	def show_info(self, id):
-		nvi = self.list_nv[id]
-		print("-"*50)
-		print("\t Nhân viên thứ: ", id+1)
-		print("\t Họ và tên: ", nvi.name)
-		print("\t Năm sinh: ", nvi.birth)
-		print("\t Vị trí công việc hiện tại: ", nvi.possition)
-		print("\t Năm bắt đầu công việc: ", nvi.start)
-		print("\t Các kỹ năng hiện có: ", ", ".join(nvi.skill))
-		print("\t Các ngôn ngữ lập trình đã từng dùng: ", ", ".join(nvi.language))
-		print("\t Các project đã hoàn thành: ", ", ".join(nvi.project))
-		print("-"*50)
-
-	def query_nv(self, nv1, nv2):
-		if len(nv1.skill) == len(nv2.skill):
-			if nv1.exp == nv2.exp:
-				return nv1.name.lower() < nv2.name.lower()
-			return nv1.exp > nv2.exp
-		return len(nv1.skill) > len(nv2.skill)
-	
-	def sort_nv(self):
-		list_ans = []
-
-		for i in self.list_nv:
-			if len(i.skill) > 3:
-				list_ans.append(i)
-
-		if len(list_ans) == 0:
-			return list_ans
-
-		for i in range(len(list_ans)):
-			for j in range(len(list_ans)-1, i, -1):
-				if self.query_nv(list_ans[i], list_ans[j]):
-					list_ans[i], list_ans[j] = list_ans[j], list_ans[i]
-
-		return list_ans
-
-	def sort_ns(self):
-		list_ans1 = []
-		list_ans2 = []
-
-		for i in self.list_nv:
-			i.language = [x.lower() for x in i.language]
-			if "python" in i.language:
-				list_ans1.append(i)
-			if 2021-i.birth > 30 and len(i.project) > 5:
-				list_ans2.append(i)
-
-		list_ans1 = sorted(list_ans1, key = lambda x: len(x.project), reverse = True)
+	def get_employee(self, id):
+		employee = self.list_employee[id]
 		
-		return [list_ans1, list_ans2]		
+		print("-"*50)
+		print(f"""
+\t Nhân viên thứ: {id + 1}
+\t Họ và tên: {employee.name}
+\t Năm sinh: {employee.birth}
+\t Vị trí công việc: {employee.position}
+\t Các kỹ năng hiện có: {", ".join(employee.skill)}
+\t Năm bắt đầu công việc: {employee.start_year}
+\t Số năm kinh nghiệm: {employee.exp}
+		""")
+		print("-"*50)
+	
+	def query(self, employee1, employee2):
+		if len(employee1.skill) == len(employee2.skill):
+			if employee1.exp == employee2.exp:
+				return employee1.name.lower() < employee2.name.lower()
+			return employee1.exp > employee2.exp 
+		return len(employee1.skill) > len(employee2.skill)
+	
+	def get_employee_for_learn(self):
 
-	def get_ans_nv(self):
-		ans = self.sort_nv()
+		employees = self.list_employee
 
-		if len(ans) == 0:
-			print("Không có nhân viên nào thỏa mãn yêu cầu")
-			print("-"*50)
-			return;
+		# Bubble sort
+		for i in range(len(employees)):
+			for j in range(len(employees)-1, i, -1):
+				if self.query(employees[i], employees[j]):
+					employees[i], employees[j] = employees[j], employees[i]
+		
+		for i in range(min(3, len(employees))):
+			self.get_employee(employees[i].id)
 
-		for i in range(min(3, len(ans))):
-			self.show_info(ans[i].id)
+class Project_Employee:
+	list_employee = []
 
-	def get_ans_ns(self):
-		ans = self.sort_ns()
+	def add_employee(self, info_employee):
+		self.list_employee = info_employee
 
-		return ans
+	def get_employee(self, id):
+		employee = self.list_employee[id]
+		
+		print("-"*50)
+		print(f"""
+\t Nhân sự thứ: {id + 1}
+\t Họ và tên: {employee.name}
+\t Năm sinh: {employee.birth}
+\t Vị trí công việc: {employee.position}
+\t Các kỹ năng hiện có: {", ".join(employee.skill)}
+\t Năm bắt đầu công việc: {employee.start_year}
+\t Số năm kinh nghiệm: {employee.exp}
+\t Các ngôn ngữ lập trình đã từng dùng: {", ".join(employee.language)}
+\t Các project đã hoàn thành: {", ".join(employee.project)}
+		""")
+		print("-"*50)
+	
+	def get_employee_python_dev(self):
+		employees = []
 
-def control(leader):
-    print("[1] Nhập thông tin nhân viên")
-    print("[2] Xem thông tin nhân viên")
-    print("[3] Danh sách 3 nhân viên tham gia khóa học phát triển toàn diện")
-    print("[4] Danh sách nhân viên đã sử dụng Python và có nhiều project hoàn thành nhất")
-    print("[5] Danh sách nhân viên từ 30 tuổi trở lên và cố số lượng project lớn hơn 5")
-    print("[6] Thoát chương trình")
-    n = int(input())
-    if n > 6 or n < 1:
-    	print("Yêu cầu không hợp lệ. Vui lòng thử lại!")
-    	control(leader)
-    else:
-    	if n == 6:
-    		return;
-    	if n == 1:
-            for i in range(leader.n):
-                leader.enter_nv(i)
-            control(leader)
-    	elif n == 2:
-	        print("Nhập id nhân viên muốn xem. Nhập 0 nếu muốn xem thông tin tất cả nhân viên: ",end="")
-	        id_nv = int(input())
-	        if id_nv < 0 or id_nv > leader.n:
-	        	print("Id nhân viên không có trong danh sách")
-	        	control(leader)
-	        if id_nv == 0:
-	        	for i in range(leader.n):
-	        		leader.show_info(i)
-	        else:
-	        	leader.show_info(id_nv-1)
-	        print("=====================================================================")
-	        control(leader)
-    	elif n == 3:
-	        leader.get_ans_nv()
-	        print("=====================================================================")
-	        control(leader)
-    	elif n == 4:
-	        ans = leader.get_ans_ns()
+		for employee in self.list_employee:
+			if "Python" in employee.language:
+				employees.append(employee)
+		
+		employees = sorted(employees, key = lambda employee: len(employee.project), reverse = True)
 
-	        if len(ans[0]) == 0:
-	        	print("Không có nhân viên nào thỏa mãn yêu cầu")
-	        	control(leader)
+		for employee in employees:
+			self.get_employee(employee.id)
 
-	        for i in ans[0]:
-	        	leader.show_info(i.id)
-	        print("=====================================================================")
-	        control(leader)
-    	elif n == 5:
-	        ans = leader.get_ans_ns()
+	def get_employee_exp(self):
+		employees = []
 
-	        if len(ans[1]) == 0:
-	        	print("Không có nhân viên nào thỏa mãn yêu cầu")
-	        	control(leader)
+		for employee in self.list_employee:
+			if employee.exp > 30 and len(employee.project) > 5:
+				employees.append(employee)
+		
+		for employee in employees:
+			self.get_employee(employee.id)
+	
+def control(learn_emp, project_emp):
+	
+	print(f"""
+[*] Thông tin về nhân viên
+\t [1] Xem thông tin cơ bản của nhân viên
+\t [2] Danh sách nhiều nhất 3 nhân viên tham giá khóa học toàn diện
+[*] Thông tin về nhân sự
+\t [3] Xem thông tin cơ bản của nhân sự
+\t [4] Danh sách nhân sự từng dùng Python và có nhiều project hoàn thành nhất
+\t [5] Danh sách nhân sự trên 30 tuổi và có số project hoàn thành lớn hơn 5
+[*] Chương trình
+\t [6] Thoát khỏi chương trình
+	""")
+	print("Nhập tùy chọn: ", end="")
+	n = int(input())
 
-	        for i in ans[1]:
-	        	leader.show_info(i.id)
-	        print("=====================================================================")
-	        control(leader)
+	if n < 1 or n > 6:
+		control(learn_emp, project_emp)
+	
+	if n == 1:
+		print("Nhập id nhân viên muốn xem (nhập 0 để xem tất cả): ", end="")
+		id_employee = int(input())
+		
+		if id_employee < 0 or id_employee > len(learn_emp.list_employee):
+			print("Id nhân viên không có trong danh sách")
+			control(learn_emp, project_emp)
+		elif id_employee == 0:
+
+			for i in range(len(learn_emp.list_employee)):
+				learn_emp.get_employee(i)
+		else:
+			learn_emp.get_employee(id_employee-1)
+
+		control(learn_emp, project_emp)
+
+	elif n == 2:
+		learn_emp.get_employee_for_learn()
+		control(learn_emp, project_emp)
+	
+	elif n == 3:
+		print("Nhập id nhân sự muốn xem (nhập 0 để xem tất cả): ", end="")
+		id_employee = int(input())
+		
+		if id_employee < 0 or id_employee > len(project_emp.list_employee):
+			print("Id nhân sự không có trong danh sách")
+			control(learn_emp, project_emp)
+		elif id_employee == 0:
+
+			for i in range(len(project_emp.list_employee)):
+				project_emp.get_employee(i)
+		else:
+			project_emp.get_employee(id_employee-1)
+
+		control(learn_emp, project_emp)
+
+	elif n == 4:
+		project_emp.get_employee_python_dev()
+		control(learn_emp, project_emp)
+	
+	elif n == 5:
+		project_emp.get_employee_exp()
+		control(learn_emp, project_emp)
 
 if __name__ == "__main__":
-	print("Vui lòng nhập số lượng nhân viên: ", end="")
+	learn_emp = Learn_Employee() # Employee for learn
+	project_emp = Project_Employee() # Employee for project
 
-	n_nhanvien = int(input())
-	leader = NhanVien(n_nhanvien)
+	current_year = datetime.now().date().year
 
-	control(leader)
+	# Nhập thông tin nhân viên
+
+	emp1 = Employee_Info(id=0, name="Nguyen Van A", birth =2000, position="Dev", skill=["math", "dev"], start_year=2010, exp =current_year-2010)
+	emp2 = Employee_Info(id=1, name="Nguyen Van B", birth =1900, position="Dev", skill=["math", "dev"], start_year=1950, exp =current_year-1950)
+	emp3 = Employee_Info(id=2, name="Nguyen Van C", birth =1950, position="Dev", skill=["math", "dev", "programing"], start_year=1970, exp =current_year-1970)
+	emp4 = Employee_Info(id=3, name="Nguyen Van D", birth =2009, position="Dev", skill=["math", "dev"], start_year=2019, exp =current_year-2019)
+	emp5 = Employee_Info(id=4, name="Nguyen Van E", birth =2015, position="Dev", skill=["math", "dev"], start_year=2020, exp =current_year-2020)
+	emp6 = Employee_Info(id=5, name="Nguyen Van F", birth =1969, position="Dev", skill=["math", "dev", "hack", "security"], start_year=1980, exp =current_year-1980)
+
+	learn_emp.add_employee([emp1, emp2, emp3, emp4, emp5, emp6])
+
+	# Nhập thông tin nhân sự	
+
+	emp1 = Employee_Info(id=0, name="Nguyen Van A", birth =2000, position="Dev", skill=["math", "dev"], start_year=2010, exp =current_year-2010, language=["Python", "C++", "Java"], project=["a", "b", "c"])
+	emp2 = Employee_Info(id=1, name="Nguyen Van B", birth =1900, position="Dev", skill=["math", "dev"], start_year=1950, exp =current_year-1950, language=["Python", "Java"], project=["b", "c"])
+	emp3 = Employee_Info(id=2, name="Nguyen Van C", birth =1950, position="Dev", skill=["math", "dev", "programing"], start_year=1970, exp =current_year-1970, language=["Python", "C++", "Java"], project=["a", "b", "c", "d", "e", "f"])
+	emp4 = Employee_Info(id=3, name="Nguyen Van D", birth =2009, position="Dev", skill=["math", "dev"], start_year=2019, exp =current_year-2019, language=["Java"], project=["a"])
+	emp5 = Employee_Info(id=4, name="Nguyen Van E", birth =2015, position="Dev", skill=["math", "dev"], start_year=2020, exp =current_year-2020, language=["C++", "Java"], project=["a", "b"])
+	emp6 = Employee_Info(id=5, name="Nguyen Van F", birth =1969, position="Dev", skill=["math", "dev", "hack", "security"], start_year=1980, exp =current_year-1980, language=["Python", "C++", "Java"], project=["a", "b", "c", "e", "f", "g", "h"])
+
+	project_emp.add_employee([emp1, emp2, emp3, emp4, emp5, emp6])
+
+	control(learn_emp, project_emp)
