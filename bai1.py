601664823f9857dd721e37d776d0c7c0f1e5a2ab
Phucvan <phucvan.maicogroup@gmail.com>
2021-11-08 13:40:09 +0700

    Marchine Learning Library

--- bai1.py
+++ bai1.py
@@ -8,42 +8,26 @@ def prime(n):
 	return all(n%i for i in range(3, int(sqrt(n) + 1), 2))
 
 
-def task_a():
-	a = np.random.randint(10, size = (3, 4))
-	b = np.random.randint(10, size = (3, 4))
-	print("A:\n", a, "\nB:\n", b)
-
-
-def task_b():
-	a = np.random.randint(10, size = (4, 4))
-	b = ~(a%2) + 2
-	print("A:\n", a, "\nB:\n", b)
-
-
-def task_c():
-	a = np.random.randint(10, size = (5, 5))
-	print("A:\n", a)
-
-	a = np.fliplr(a)
-	func = np.vectorize(prime)
-	a_prime_diagonal = func(a.diagonal())
-	count_prime_diagonal = len(np.extract(a_prime_diagonal, a))
-	print("A flip left to right: \n", a, "\nCount prime in diagonal: ", count_prime_diagonal)
-
-
 if __name__ == "__main__":
 	# A
-	print("Task A:\n")
-	task_a()
-	print("\nEnd task A\n")
+	MATRIX_1 = np.random.randint(10, size = (3,4))
+	MATRIX_2 = np.random.randint(10, size = (4,4))
+	print(MATRIX_1)
+	print(MATRIX_2)
+	print(MATRIX_1@MATRIX_2)
 
 	#B
-	print("Task B:\n")
-	task_b()
-	print("\nEnd task B\n")
+	MATRIX_A = MATRIX_2.copy()
+	MATRIX_B = ~(MATRIX_A%2) + 2
+	print(MATRIX_A)
+	print(MATRIX_B)
 
 	#C
-	print("Task C:\n")
-	task_c()
-	print("\nEnd task C\n")
+	A_FLIP = np.fliplr(MATRIX_A)
+	func = np.vectorize(prime)
+	DIAG_A_FLIP = A_FLIP.diagonal()
+	count_prime = len(np.extract(func(DIAG_A_FLIP), A_FLIP))
+	print(DIAG_A_FLIP)
+	print(count_prime)
+
 	
